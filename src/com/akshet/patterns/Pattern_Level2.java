e5c41b25fc5d7415ce639d4aabd0ec4cdeb739f9
AKSHET PATIAL <akshet.patial@gmail.com>
2023-03-23 02:01:12 +0530

    Created the Pattern_Level2.java file

---
+++ src/com/akshet/patterns/Pattern_Level2.java
@@ -0,0 +1,13 @@
+package com.akshet.patterns;
+
+import java.util.Scanner;
+
+public class Pattern_Level2 {
+
+    public static void main(String[] args) {
+        System.out.println("Enter the number of rows you want to print the pattern! ");
+        Scanner scan = new Scanner(System.in);
+        int rows= scan.nextInt();
+
+    }
+}
